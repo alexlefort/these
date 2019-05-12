03ddccff4c5d3fda9de2aa2f23d06492fdb28166
alexandre-lefort <alexandre.lefort09@gmail.com>
2019-05-12 19:49:05 +0200

    ajout modeles lineairex

---
+++ toolbox/models/tbx_sm_depth_model_one_plane_slope.m
@@ -0,0 +1,48 @@
+function model = tbx_sm_depth_model_one_plane_slope(p)
+
+    Cz = [0 0 p.CzW/p.Vs p.CzQ*p.L/p.Vs p.CzB1 0];
+    Cm = [0 0 p.CmW/p.Vs p.CmQ*p.L/p.Vs p.CmB1 0];
+
+    Fz_navire = p.Vs^2/(2*p.CRemp*p.L)*Cz;
+    My_navire = p.Vs^2/(2*p.CRemp)*Cm;
+
+    Fz_couplage = [0 0 0       p.mu*p.Vs 0 0];
+    My_couplage = [0 0 0 -p.mu*p.Xg*p.Vs 0 0];
+
+    Fz_hydrostat = [0              0 0 0 0   (p.mu-1)*p.g];
+    My_hydrostat = [0 -p.mu*p.Zg*p.g 0 0 0 -p.mu*p.Xg*p.g];
+
+    Fz = Fz_hydrostat + Fz_couplage + Fz_navire;
+    My = My_hydrostat + My_couplage + My_navire;
+
+    Mpropre  = [      p.mu   -p.mu*p.Xg ;...
+                -p.mu*p.Xg p.L^2*p.X_2] ;
+                
+    Majoutee = [      p.Mu_3       -p.L*p.Nu_35 ;...
+                -p.L*p.Nu_35  p.L^2*p.Lambda_2] ;
+                 
+    Masse = Mpropre + Majoutee;
+
+    Aux = Masse\[Fz ; My];
+    
+    Wpt = Aux(1,:);
+    Qpt = Aux(2,:);
+    
+    thetapt = [0     0 0 1 0 0];
+    Zpt     = [0 -p.Vs 1 0 0 0];
+
+    F = [Zpt ; thetapt ; Wpt ; Qpt];
+    A = F(:,1:4);
+    B = F(:,5);
+
+    %% Conversion (Z Theta W Q) -> (Z Theta Pi Q)
+
+    ZThetaPiQ = [1 0 0 0 ; 0 1 0 0 ; 0 1 -1/p.Vs 0 ; 0 0 0 1]; %% Transformee en X = [Theta, Pente, Q];
+
+    A = ZThetaPiQ*A*inv(ZThetaPiQ);
+    B = ZThetaPiQ*B;
+    
+    model.a = A;
+    model.b = B;
+    model.c = eye(4);
+    model.d = zeros(4,1);
