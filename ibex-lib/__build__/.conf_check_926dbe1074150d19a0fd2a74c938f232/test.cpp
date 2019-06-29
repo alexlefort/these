db54fbaafdc19bccc76ecf84ff9bbb366ce777be
alexandre-lefort <alexandre.lefort09@gmail.com>
2019-06-29 19:38:03 +0200

    Untracked files issue resolved to fix .gitignore

---
+++ ibex-lib/__build__/.conf_check_926dbe1074150d19a0fd2a74c938f232/test.cpp
@@ -0,0 +1,7 @@
+
+							#include "coin/ClpSimplex.hpp"
+							int main (int argc, char* argv[]) {
+								ClpSimplex 	myclp;
+								myclp.resize(2,2);
+								return 0;
+							} 
---
+++ ibex-lib/__build__/.conf_check_fd0c6bded51b89f8a1cf1614b1f72e6a/test.cpp
@@ -0,0 +1,8 @@
+
+								#include "ilcplex/cplex.h"
+								int main (int argc, char* argv[]) {
+									CPXENVptr  envcplex;
+									CPXLPptr lpcplex;
+									return 0;
+								}
+							
